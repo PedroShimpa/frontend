5e4d0d60b9b1abbeaf8b57e95f8d1cbd421dce30
Pedro Falconi <pedrofalck2000@gmail.com>
2024-06-04 09:50:54 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import OrderList from './components/OrderList';
 import CreateOrder from './components/CreateOrder';
-import UpdateOrder from './components/UpdateOrder';
-import DeleteOrder from './components/DeleteOrder';
 import { Container, Typography } from '@mui/material';
 
 function App() {
